f72baed253630635cee86969679e38e8cb4e690d
Jason Rust <rustyparts@gmail.com>
2003-01-09 22:43:51 +0000

    * Update message constants to work with new actions

--- lib/constants.inc.php
+++ lib/constants.inc.php
@@ -1,5 +1,5 @@
 <?php
-/** $Id: constants.inc.php,v 1.1 2003/01/03 22:42:43 jrust Exp $ */
+/** $Id: constants.inc.php,v 1.2 2003/01/09 22:43:51 jrust Exp $ */
 if (!defined('IN_FASTFRAME')) {
     die('Hacking Attempt');
 }
@@ -23,9 +23,11 @@ define('LOGOUT_SUBMIT',          12, true);
 define('DISPLAY_MAIN',           13, true);
 
 /**
- * Message constants.  These images need to be in the graphics/actions directory
+ * Message constants.  These images need to be in the graphics/alerts directory.
+ * The theme can mimic the grapics/alerts directory to override the default image.
  */
 define('FASTFRAME_NORMAL_MESSAGE', 'message.gif', true);
 define('FASTFRAME_ERROR_MESSAGE', 'error.gif', true);
-define('FASTFRAME_SEARCH_MESSAGE', 'search.gif', true);
+define('FASTFRAME_WARNING_MESSAGE', 'warning.gif', true);
+define('FASTFRAME_SUCCESS_MESSAGE', 'success.gif', true);
 ?>
