c0352ea99e138eee21e0f6eaa99d81d50a1a981d
Jason Rust <rustyparts@gmail.com>
2003-01-22 02:01:46 +0000

    * Some cleanups.

--- lib/FastFrame/Perms/PermSource.php
+++ lib/FastFrame/Perms/PermSource.php
@@ -1,8 +1,12 @@
 <?php
+/** $Id: PermSource.php,v 1.3 2003/01/22 02:01:46 jrust Exp $ */
 // {{{ class  PermSource
 
 /**
  * Abstract class describing a source pull permissions from
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lesser.html.
  *
  * @version Revision: 1.0 
  * @author  Greg Gilbert <ggilbert@brooks.edu
@@ -13,11 +17,31 @@
 // }}}
 class PermSource {
     // {{{ properties
+
     /**
      * The name assigned to this source
-     * @var string $sourceName
+     * @type string
      */
     var $sourceName;
+
+    // }}}
+    // {{{ constructor
+    
+    /**
+     * Initialize the AuthSource class
+     *
+     * Create an instance of the AuthSource class.  
+     *
+     * @param string $in_name The name of this auth source
+     * @param array $in_params Additional parameters needed for authenticating
+     *
+     * @access public
+     * @return object AuthSource object
+     */
+    function AuthSource($in_name, $in_params)
+    {
+        $this->sourceName = $in_name;
+    }
 
     // }}}
     // {{{ create()
@@ -33,59 +57,54 @@ class PermSource {
      * @access public
      * @return object Subclass of PermSource for the specified type, or PermSource if none is found 
      */
-    function &create($type, $name, $params)
+    function &create($in_type, $in_name, $in_params)
     {
-        @include_once(dirname(__FILE__) . '/' . $type . ".php");
-				
-        $permClass= "PermSource_" . $type;
-				
-
-        if ( class_exists($permClass)) {
-            $object = &new $permClass( $name, $params);
+        $s_permFile = dirname(__FILE__) . '/' . $in_type . '.php';
+        // we assume they don't want to use perms in this case
+        if (empty($in_type)) {
+            return true;
+        }
+        elseif (!@file_exists($s_authFile)) {
+            return FastFrame::fatal("$in_type is an invalid permission type.", __FILE__, __LINE__);
         }
         else {
-            $object = &new PermSource( $name, $params);
+            include_once $s_authFile;
+        }
+				
+        $s_permClass= 'PermSource_' . $type;
+				
+        if (class_exists($s_permClass)) {
+            $o_perms =& new $s_permClass($name, $params);
+        }
+        else {
+            $o_perms =& new PermSource($name, $params);
         }
 
-        return $object; 
-    }
-
-    // }}}
-    // {{{ constructor
-    
-    /**
-     * Initialize the AuthSource class
-     *
-     * Create an instance of the AuthSource class.  
-     *
-     * @access public
-     * @return AuthSource object
-     */
-    function AuthSource($name, $params)
-    {
-        $this->sourceName = $name;
+        return $o_perms; 
     }
 
     // }}}
     // {{{ getAppList()
+
     /**
-     *
      * Get a list of apps that the user has permission to log into
      *
      * This function returns an array applications that the user has
      * been granted permission to log into
      *
-     * @access	
-     * @see		??
+     * @param string $in_user The user ID
+     *
+     * @access public
      */
     function getAppList($in_user)
     {
         return array();
     }
+
     // }}}
     // {{{ getGroupList()
+
     /**
-     *
      * Get a list of groups the user is a member of
      *
      * This function returns the list of groups that the specified user
@@ -93,14 +112,16 @@ class PermSource {
      *
      * @access public
      */
-    function getGroupList ( $in_userID, $in_app)
+
+    function getGroupList($in_userID, $in_app)
     {
         return array();
     }
+
     // }}}
     // {{{ getPerms()
+
     /**
-     *
      * Returns a list of permissions the user has
      *
      * This function returns the permissions that the specified user
@@ -108,10 +129,11 @@ class PermSource {
      *
      * @access public
      */
-    function getPerms($in_userID,  $in_appName )
+    function getPerms($in_userID, $in_appName)
     {
         return array();
     }
+
     // }}}
     // {{{ getName()
 
@@ -127,6 +149,7 @@ class PermSource {
     {
         return $this->_sourceName;
     }
+
     // }}}
 }
 ?>
--- lib/FastFrame/Perms/sql.php
+++ lib/FastFrame/Perms/sql.php
@@ -1,8 +1,12 @@
 <?php
+/** $Id: sql.php,v 1.3 2003/01/22 02:01:46 jrust Exp $ */
 // {{{ class  PermSource_sql
 
 /**
  * A Permissions source that reads it's data from an SQL database
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you
+ * did not receive this file, see http://www.fsf.org/copyleft/lesser.html.
  *
  * @version Revision: 1.0 
  * @author  Greg Gilbert <ggilbert@brooks.edu
@@ -13,57 +17,62 @@
 // }}}
 class PermSource_sql {
     // {{{ properties
+
     /**
      * The name assigned to this source
-     * @var string $sourceName
+     * @type string
      */
     var $sourceName;
 
-    // }}}
-    // {{{ constructor
-    
     /**
      * The DSN that we will use to connect to our database
      * @access	private
      */
     var $_dsn;
+
+    // }}}
+    // {{{ constructor
+
     /**
      * Initialize the PermSource class
      *
      * Create an instance of the PermSource_sql class, 
      *
+     * @param string $in_name The name of this auth source
+     * @param array $in_params Additional parameters needed for authenticating
+     *
      * @access public
-     * @return PermSource_sql object
+     * @return object PermSource_sql object
      */
-    function PermSource_sql($name, $params)
+    function PermSource_sql($in_name, $in_params)
     {
-        $this->sourceName = $name;
-        $this->_dsn = $s_type . '://' . $params['username'] . ':' . $params['password'] . '@' . $params['host'] . '/' . $params['database'];
-
+        $this->sourceName = $in_name;
+        $this->_dsn = $s_type . '://' . $in_params['username'] . ':' . $in_params['password'] . '@' . $in_params['host'] . '/' . $in_params['database'];
     }
 
     // }}}
     // {{{ getAppList()
+
     /**
-     *
      * Get a list of apps that the user has permission to log into
      *
      * This function returns an array applications that the user has
      * been granted permission to log into
      *
-     * @access	
-     * @see		??
+     * @param string $in_user The user ID
+     *
+     * @access public
      */
     function getAppList($in_user)
     {
         $o_db->_db =& $this->_openDatabase();
-        $s_sql=' SELECT `perms`.`perm` FROM `perms`, `groups` WHERE
+        $s_sql = ' SELECT `perms`.`perm` FROM `perms`, `groups` WHERE
             `groups`.`group`=`perms`.`group`
             AND `perms`.`app`=`groups`.`app`
             AND `perms`.`perm`=`login` 
-            AND `groups`.`user`=' . quote( $this->_userid);
+            AND `groups`.`user`=' . quote($this->_userid);
 
-        $a_result=$o_db->getCol($s_sql,'perm');
+        $a_result = $o_db->getCol($s_sql,'perm');
 
         if (DB::isError($a_result)) {
             FastFrame::fatal($a_result, __FILE__, __LINE__);
@@ -74,8 +83,8 @@ class PermSource_sql {
     }
     // }}}
     // {{{ getGroupList()
+
     /**
-     *
      * Get a list of groups the user is a member of
      *
      * This function returns the list of groups that the specified user
@@ -83,14 +92,15 @@ class PermSource_sql {
      *
      * @access public
      */
-    function getGroupList ( $in_userID, $in_app)
+    function getGroupList ($in_userID, $in_app)
     {
         return array();
     }
+
     // }}}
     // {{{ getPerms()
+
     /**
-     *
      * Returns a list of permissions the user has
      *
      * This function returns the permissions that the specified user
@@ -100,70 +110,70 @@ class PermSource_sql {
      */
     function getPerms($in_userID,  $in_appName )
     {
-        $a_allowed=$this->getPermissions($in_userID, $in_appName, false);
-        $a_denied=$this->getPermissions($in_userID, $in_appName, true);
+        $a_allowed = $this->getPermissions($in_userID, $in_appName, false);
+        $a_denied = $this->getPermissions($in_userID, $in_appName, true);
         return array_diff($a_allowed, $a_denied);
     }
+
     // }}}
     // {{{ _getPermissions()
-        /**
-         *
-         * List denied permissions
-         *
-         * This function returns an array containing the permissions that have been 
-         * explicitly denied or granted to the user.
-         *
-         * @access  private
-         */
-        function _getPermissions($in_userID, $in_app, $in_denied)
-        {
-            $o_db=&$this->_openDatabase();
 
-            if ($in_denied) {
-                $i_getDenied=1;
-            }
-            else {
-                $i_getDenied=0;
-            }
+    /**
+     * List denied permissions
+     *
+     * This function returns an array containing the permissions that have been 
+     * explicitly denied or granted to the user.
+     *
+     * @access  private
+     */
+    function _getPermissions($in_userID, $in_app, $in_denied)
+    {
+        $o_db =& $this->_openDatabase();
 
-            $s_sql=' SELECT `perms`.`perm` FROM `perms`, `groups` WHERE
-            `groups`.`group`=`perms`.`group`
-            AND `perms`.`app`=`groups`.`app`
-            AND `groups`.`user`=' . quote( $in_userID) . '
-            AND `groups`.`app`=' . quote( $in_app) . '
-            AND `denied`=' . $i_getDenied;
-
-            $a_result=$o_db->getCol($s_sql,'perm');
-
-            if (DB::isError($a_result)) {
-                FastFrame::fatal($a_result, __FILE__, __LINE__);
-            }
-
-            $o_db->disconnect();
-            return $a_result;
-
-
+        if ($in_denied) {
+            $i_getDenied=1;
         }
+        else {
+            $i_getDenied=0;
+        }
+
+        $s_sql = ' SELECT `perms`.`perm` FROM `perms`, `groups` WHERE
+        `groups`.`group`=`perms`.`group`
+        AND `perms`.`app`=`groups`.`app`
+        AND `groups`.`user`=' . quote( $in_userID) . '
+        AND `groups`.`app`=' . quote( $in_app) . '
+        AND `denied`=' . $i_getDenied;
+
+        $a_result = $o_db->getCol($s_sql,'perm');
+
+        if (DB::isError($a_result)) {
+            FastFrame::fatal($a_result, __FILE__, __LINE__);
+        }
+
+        $o_db->disconnect();
+        return $a_result;
+    }
+
     // }}}
     // {{{ _openDatabase()
+
     /**
-     *
      * This function creates a database connection 
      *
      * This is the long description for the Class
      *
-     * @access	
-     * @see		??
+     * @access public
      */
     function &_openDatabase()
     {
-            $o_db=DB::connect($this->_dsn);
-            if (DB::isError($o_db)) {
-                FastFrame::fatal($a_dataConnection[$s_app], __FILE__, __LINE__);
-            }   
+        $o_db = DB::connect($this->_dsn);
+        if (DB::isError($o_db)) {
+            FastFrame::fatal($a_dataConnection[$s_app], __FILE__, __LINE__);
+        }   
 
-            return $o_db;
+        return $o_db;
     }
+
     // }}}
 }
 
